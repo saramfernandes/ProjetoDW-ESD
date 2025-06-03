718ffd386f4bd924bed6ce06e17cd1c55660dc40
SARA MACHADO FERNANDES <sara_m_fernandes@estudante.sesisenai.org.br>
2025-06-03 20:46:49 -0300

    pilha

---
+++ src/app/estruturas/Pilha.ts
@@ -0,0 +1,33 @@
+class Pilha {
+  private topo: No | null = null;
+
+  estaVazia(): boolean {
+    return this.topo === null;
+  }
+
+  insere(nome: string): void {
+    const novoNo = new No(nome, this.topo);
+    this.topo = novoNo;
+  }
+
+  espiaTopo(): string | null {
+    return this.topo ? this.topo.nome : null;
+  }
+
+  ver(): string[] {
+    const nomes: string[] = [];
+    let aux = this.topo;
+    while (aux) {
+      nomes.push(aux.nome);
+      aux = aux.anterior;
+    }
+    return nomes;
+  }
+}
+
+class No {
+  constructor(
+    public nome: string,
+    public anterior: No | null = null
+  ) {}
+}

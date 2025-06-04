1d3c2b7ec6d73a7c5395d3b6561d4c60417b9fa9
JOÃO FORTUNATO GRIEBNER <joao_griebner@estudante.sesisenai.org.br>
2025-06-03 21:56:45 -0300

    iniciado busca component

--- src/app/layout/busca/busca.component.ts
+++ src/app/layout/busca/busca.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-busca',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './busca.component.html',
   styleUrl: './busca.component.scss'
 })
 export class BuscaComponent {
+  query: string = '';
 
+  buscar() {
+    // TODO: IMPLEMENTAR lógica de busca
+    console.log('Buscando:', this.query);
+  }
+
+  limpar() {
+    // TODO: IMPLEMENTAR lógica de limpeza
+    this.query = '';
+    console.log('Campo limpo');
+  }
 }
